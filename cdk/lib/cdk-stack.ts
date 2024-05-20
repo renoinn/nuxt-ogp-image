c57ae2f3c35105db9d3f46559dfd6e9e4313b97c
renoinn <oomori.lunch@gmail.com>
2024-05-20 14:13:12 +0900

    add cdk

---
+++ cdk/lib/cdk-stack.ts
@@ -0,0 +1,22 @@
+import { Duration, Stack, StackProps } from 'aws-cdk-lib';
+import { Function, Runtime, AssetCode, FunctionUrlAuthType } from 'aws-cdk-lib/aws-lambda';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class CdkStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const lambda = new Function(this, 'OgpImage', {
+      functionName: 'OgpImage',
+      handler: 'index.handler',
+      runtime: Runtime.NODEJS_20_X,
+      code: new AssetCode(`../.output/server/`),
+      memorySize: 512,
+      timeout: Duration.seconds(30)
+    });
+    const url = lambda.addFunctionUrl({
+      authType: FunctionUrlAuthType.NONE,
+    })
+  }
+}
